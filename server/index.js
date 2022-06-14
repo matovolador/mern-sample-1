b79942960312c7342e364477163a563fa5ff7268
Matias Garafoni <matias.garafoni@gmail.com>
2022-06-14 01:23:58 -0300

    setup mongodb as docker container

--- server/index.js
+++ server/index.js
@@ -8,3 +8,10 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+
+const CONNECTION_URL = "mongodb://root:password@localhost:5601/db?authSource=admin"
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch((error) => console.log(error.message));
