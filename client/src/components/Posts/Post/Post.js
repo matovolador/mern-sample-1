19327a3de20ad2803f756d0b1a2bcbfc9ceb6926
Matias Garafoni <matias.garafoni@gmail.com>
2022-06-27 18:29:03 -0300

    like and delete

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,15 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import {useDispatch} from 'react-redux';
 
 import useStyles from './styles';
 
+import { deletePost, likePost } from '../../../actions/posts'
+
 const Post = ( {post, setCurrentId} ) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -30,12 +34,12 @@ const Post = ( {post, setCurrentId} ) => {
                 <Typography variant="h6"  gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon frontSize='small' />
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon frontSize='small' />
                     Delete
                 </Button>
